ace55ced161e4fa298cc4332bcb9b4b0de5ac094
wilson cely <wectcorp@gmail.com>
2022-06-04 14:41:10 +0200

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :musicians do
+    resources :invitations, only: [:new, :create]
+  end
+
+  resources :orchestras do
+    resources :posts, only: [:new, :create]
+    resources :invitations, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
