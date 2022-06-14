a74b8cf3fcb09a399f2b292bf055cb2d9cf3ce64
HAITAM EL OUANNAS <haitamelouannas@gmail.com>
2022-06-14 20:01:56 +0200

    devise new redirection roots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  def after_sign_in_path_for(resource)
+    if @musician = Musician.find_by(user_id: current_user.id)
+      stored_location_for(resource) || orchestras_path
+    elsif @orchestra = Orchestra.find_by(user_id: current_user.id)
+      stored_location_for(resource) || musicians_path
+    else
+      stored_location_for(resource) || root_path
+    end
+  end
 end
