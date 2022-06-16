7c86ba32a67437e013b88f549099318c88aa22f4
Charles GrangerTheveniau <charlestheveniau@gmail.com>
2022-06-16 21:15:37 +0200

    update seed v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ user_musician = User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     birthday: Faker::Date.between(from: '1948-09-23', to: '2000-09-25'),
-    level: [1 ,2, 3, 4, 5].sample,
+    level: ["Beginner", "Intermediate", "Advanced"].sample,
     instrument: ["Violin", "Viola", "Cello", "Double Bass", "Harp", "Flute", "Piccolo", "English Horn", "Clarinet", "Saxophone", "Trumpet", "Trombone"].sample,
     style: ["Classic","Pop","Electronic", "Jazz"].sample,
     photo: "https://picsum.photos/200/300",
@@ -52,7 +52,7 @@ end
     size: [5, 15,10, 4 , 20].sample,
     zip_code: [75000, 75001, 75002, 75003, 75004, 75005, 75006, 75007, 75008, 75009, 75010, 75011, 75012, 75013, 75014, 75015, 75016, 75017, 75018, 75019, 75020].sample,
     frequency: "Rehearsals every #{['Monday', "Tuesday", "Wednesday", "Thursday", "Sunday"].sample} at 7 PM",
-    description: Faker::Lorem.sentence(word_count: 10),
+    description: Faker::Lorem.sentence(word_count: 30),
     name: Faker::Music.band,
     bio: Faker::Lorem.sentence(word_count: 6),
     user_id: user_orchestra.id
