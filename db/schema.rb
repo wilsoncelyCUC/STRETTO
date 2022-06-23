885086a2c2e67d56c697f9e4fe21e728fc3de083
wilson cely <wectcorp@gmail.com>
2022-06-24 01:15:03 +0200

    added mobile and correction of bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_21_201959) do
+ActiveRecord::Schema.define(version: 2022_06_23_213417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_06_21_201959) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "phone_number"
     t.index ["user_id"], name: "index_musicians_on_user_id"
   end
 
@@ -81,6 +82,7 @@ ActiveRecord::Schema.define(version: 2022_06_21_201959) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "phone_number"
     t.index ["user_id"], name: "index_orchestras_on_user_id"
   end
 
